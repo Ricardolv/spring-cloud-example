8536831476c675776f3735ef75f0f08bb1c9ed9b
Ricardolv <richardluizv@gmail.com>
2021-01-16 21:21:33 -0300

    adicionando servidor de autenticacao

---
+++ auth/src/main/java/com/richard/microservice/auth/config/AuthorizationServerCofig.java
@@ -0,0 +1,48 @@
+package com.richard.microservice.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+public class AuthorizationServerCofig extends AuthorizationServerConfigurerAdapter {
+	
+	
+	private final AuthenticationManager authenticationManager;
+	private final UserDetailsService userDetailsService;
+	private final PasswordEncoder passwordEncoder;
+
+	public AuthorizationServerCofig(AuthenticationManager authenticationManager,
+			UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		this.authenticationManager = authenticationManager;
+		this.userDetailsService = userDetailsService;
+		this.passwordEncoder = passwordEncoder;
+	}
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+		clients.inMemory()
+			.withClient("loja")
+			.secret(passwordEncoder.encode("lojapwd"))
+			.authorizedGrantTypes("password")
+			.scopes("web", "mobile");
+	
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+		endpoints.authenticationManager(authenticationManager)
+		.userDetailsService(userDetailsService);
+		
+		
+		
+		
+	}
+	
+}
